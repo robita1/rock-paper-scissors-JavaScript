5a9fc2ac8d7ae36275f2225c51b1f2d3783fd286
rob <robasmerino12@gmail.com>
2024-06-24 20:28:20 +0100

    -create getComputerChoice function
    -Implement the function to return 'rock','paper', or 'scissors' randomly to ensureunpredictability in the game

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+function getComputerChoice(){
+    const computer_choice = ['rock', 'paper', `scissors`];
+    return computer_choice[Math.floor(Math.random() * computer_choice.length)]; 
+}
+console.log(getComputerChoice());
 function getHumanChoice(){
     const user =  prompt("Pick your desired outcome:Rrock, Paper, or Scissors ? ").toLowerCase();
     return user;
