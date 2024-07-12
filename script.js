c1aff7bbaf974798bd44e2be27a596bc8c9f1415
rob <robasmerino12@gmail.com>
2024-07-12 14:41:14 +0100

    -display running score and add <br>
    -add running score display with <br> for better formatting

--- script.js
+++ script.js
@@ -17,30 +17,31 @@ function playRound(humanChoice, computerChoice){
     let message ="";
 
     const result = document.querySelector(".result");
-    result.innerHTML = message;
 
-    message += (`Computer has picked: ${computerChoice}`);
-    message += (`User has picked: ${humanChoice}`);
+    message += (`Computer has picked: ${computerChoice}<br>`);
+    message += (`User has picked: ${humanChoice}<br>`);
 
     if (humanChoice === computerChoice) {
-        message += ("It's a draw!");
+        message += ("It's a draw!<br>");
     }else if (
 (humanChoice === 'rock' && computerChoice === 'scissors') || 
 (humanChoice === 'paper' && computerChoice === 'rock') || 
 (humanChoice === 'scissors' && computerChoice === 'paper')
 ){
-    message += (`You win! ${humanChoice} beats ${computerChoice}`);
+    message += (`You win!<br> ${humanChoice} beats ${computerChoice}<br>`);
   humanScore++;
 }else if(
     (humanChoice === 'scissors' && computerChoice === 'rock') || 
     (humanChoice === 'paper' && computerChoice === 'scissors') ||
     (humanChoice === 'rock' && computerChoice === 'paper')
 ){
-    message += (`You lose! ${computerChoice} beats ${humanChoice}`);
+    message += (`You lose!<br> ${computerChoice} beats ${humanChoice}<br>`);
     computerScore++;
 }else{
     message =("Please only choice rock, paper, or sicssors")
     }
+    message +=(`humanScore: ${humanScore}, computerScore: ${computerScore} `);
+    result.innerHTML = message;
 }
 const rockButton = document.querySelector(".rock");
     rockButton.addEventListener('click', () => {
